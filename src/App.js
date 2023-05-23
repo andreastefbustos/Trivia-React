001489e7ec7e0633d8d93f15a6f4d9cd57f43e05
Andrea Bustos <andreastefbustos@gmail.com>
2023-05-23 15:54:13 -0400

    HITO 1/ Sugerencia de mi micro-proyecto C

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [question1, setQuestion1] = useState('');
+  const [question2, setQuestion2] = useState('');
+
+  const handleQuestion1Change = (event) => {
+    setQuestion1(event.target.value);
+  };
+
+  const handleQuestion2Change = (event) => {
+    setQuestion2(event.target.value);
+  };
+
+  const onClickAnswer = () => {
+    const message = `Question 1: ${question1} y Question 2: ${question2}`;
+    alert(message);
+  };
+
+return (
+      <React.Fragment>
+        <h1>1. Cuál es la Flor Nacional de Venezuela?</h1>
+        <label>
+          <input 
+            type="radio"  
+            value="option1"
+            checked={question1 === "option1"}
+            onChange={handleQuestion1Change} 
+          />
+          Orquídea
+        </label>
+        <label>
+          <input 
+            type="radio"  
+            value="option2"
+            checked={question1 === "option2"}
+            onChange={handleQuestion1Change} 
+          />
+          Girasol
+        </label>
+        <label>
+          <input 
+            type="radio" 
+            value="option3"
+            checked={question1 === "option3"}
+            onChange={handleQuestion1Change}
+          />
+          Hortensia
+        </label>
+
+        <h1>2. Cuál es el clima que más predomina en México?</h1>
+        <label>
+          <input 
+            type="radio"  
+            value="option1"
+            checked={question2 === "option1"}
+            onChange={handleQuestion2Change} 
+          />
+          Templado
+        </label>
+        <label>
+          <input 
+            type="radio"  
+            value="option2"
+            checked={question2 === "option2"}
+            onChange={handleQuestion2Change} 
+          />
+          Templado-Sub-húmedo
+        </label>
+        <label>
+          <input 
+            type="radio"
+            value="option3"
+            checked={question2 === "option3"}
+            onChange={handleQuestion2Change}
+          />
+          Sub-húmedo
+        </label>
+
+        {/* <h1>3. Cúal es la ciudad originaria de los Beatles?</h1>
+        <label>
+          <input type="radio" name="answer3" value="a" />Bristol, United Kingdom
+        </label>
+        <label>
+          <input type="radio" name="answer3" value="b" />Londres, United Kingdom
+        </label>
+        <label>
+          <input type="radio" name="answer3" value="c" />Liverpool, United Kingdom
+        </label> */}
+
+        <button onClick={onClickAnswer}>Responder y Ver Resultados</button>
+
+      </React.Fragment>
+      
   );
 }
 
---
+++ src/proyectoHitos.md
@@ -0,0 +1,39 @@
+# Hito 1
+
+Comienza intentando hacer la versión más simple de una trivia.
+
+* 1 sola pantalla o vista.
+* 2 preguntas con, al menos, 3 alternativas de respuesta cada una.
+* 1 botón para responder y ver cuál es la alternativa correcta.
+* No te dice si acertaste o no, sólo te dice cuál alternativa era la correcta.
+
+## Sugerencia de micro-proyecto A: Prueba hacer el "esqueleto" estático en HTML
+
+Una interfaz básica con:
+
+* Dos preguntas con sus respectivas alternativas de respuesta en forma de radio buttons (🔘)
+* El botón para “Responder y ver resultados”.
+
+### Aprenderás:
+
+Cómo construir una página básica HTML con elementos de formulario.
+
+## Sugerencia de micro-proyecto B: Prueba darle algo de interacción
+
+Cuando la usuaria dé click en alguno de los radio button, muéstrale un mensaje de alerta (alert) en el navegador que contenga el valor (texto) del radio button cliqueado.
+
+Pista: Para mostrar un mensaje de alerta básico hay una función de JavaScript llamada “alert”. Esta función puede “escuchar” eventos del navegador como click y hacer algo cuando suceda usando onclick o addEventListener.
+
+### Aprenderás:
+
+* A detectar eventos en el navegador (los clicks de la usuaria) y hacer algo cuando sucedan.
+* Identificar los elementos HTML que hay en el navegador y obtener sus valores y/o estados.
+
+## Sugerencia de micro-proyecto C: Prueba darle interacción más cercana a la que pide el proyecto
+
+En lugar de mostrar el valor de cada radio button cuando se le hace click, que esta vez solamente se marque el radio button seleccionado y que el mensaje alert con los valores de los radio button seleccionados se muestre cuando al hacer click en el botón “Responder y ver resultados”.
+
+### Aprenderás:
+
+* A detectar eventos en el navegador (los clicks de la usuaria) .
+* Identificar los elementos HTML que hay en el navegador y obtener sus valores/estados.
